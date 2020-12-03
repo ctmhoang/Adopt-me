f2656ab1273694fc30de13e8a453e5207a07bed1
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-12-03 00:11:30 -0500

    Read props via angurments pass-in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 const App = () => {
   return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
+    "div", // tags name kind of component ?
+    undefined, // atributes {}
+    React.createElement("h1", {}, "Adopt Me!"), //children variable arity,
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havenese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+const Pet = (props) => {
+  return React.createElement(
+    "div",
+    undefined,
+    React.createElement("h1", undefined, props.name),
+    React.createElement("h2", undefined, props.animal),
+    React.createElement("h2", undefined, props.breed)
+  );
+};
+
+ReactDOM.render(
+  React.createElement(App), // Or use a composite component
+  document.getElementById("root")
+);
