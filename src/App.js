e08e369bd1de57964b4f48d47ea9385f77ab4d73
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-12-04 23:21:38 -0500

    Async call to PetFinder API, update Pet component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams/>
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
-render(
-  <App/>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
